e137873615eeb4dd0930246590e65e600f76d211
Jaime Gavilan Torrero <jgavtor562@g.educaand.es>
2025-12-07 21:40:38 +0100

    ""

--- docs/urgencias-tailwind-vite/vite.config.js
+++ docs/urgencias-tailwind-vite/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: './',
+  base: '/Hospital-Tecu-Ra---JaimeGavilanTorrero',
   build: {
     rollupOptions: {
       input: {
