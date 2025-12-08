e5d2923b32184de86c47cf294e624c3c28860767
Jaime Gavilan Torrero <jgavtor562@g.educaand.es>
2025-12-08 18:37:18 +0100

    Correccion de rutas para Github

--- hospital-frontend/enfermeria-sass-vite/vite.config.js
+++ hospital-frontend/enfermeria-sass-vite/vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/Hospital-Tecu-Ra---JaimeGavilanTorrero',
+  base: './',
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),             // página principal del módulo
-        mapa: resolve(__dirname, 'mapa-camas.html'),    
-        medicacion: resolve(__dirname, 'medicacion.html'),    
-    },
+        main: resolve(__dirname, 'index.html'),
+        mapa: resolve(__dirname, 'mapa-camas.html'),
+        medicacion: resolve(__dirname, 'medicacion.html'),
+      },
     },
   },
 })
